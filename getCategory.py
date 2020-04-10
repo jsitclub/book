7be392e3173d49379c62b2b07d6ab296f29da451
jsitclub <jsitclub@gmail.com>
2020-04-10 17:10:00 +0900

    get category

--- getCategory.py
+++ getCategory.py
@@ -3,29 +3,26 @@ from pprint import pprint
 import requests
 
 #국내소설,해외소설
-wfs=['001001001']
-result=["001001001:가정 살림"]
+wfs=['001','002']
+result=["001:국내소설","002:해외소설"]
+urlYes='http://www.yes24.com/24/Category/Display/'
+removeStr='24/Category/Display/'
 
-# 아래주소로 가서 검색할것!!!
-# http://www.yes24.com/24/Category/Display/001001004004008002
-# 만들어놓은 코드는 depth2까지만 검색!!
 
-url='http://www.yes24.com/24/category/bestseller?CategoryNumber='+wfs[0]+'&sumgb=06'
-html = requests.get(url)
-soup = bs(html.text,'html.parser')
+def GetCategory(category):
+    ## Get html of list area
+    url=urlYes+category
+    html = requests.get(url)
+    soup = bs(html.text,'html.parser')
+    cateLiArea = soup.find('div',{"class":'cateLiArea'})
 
-#### get all of menu
-lstcate = soup.select("#category"+ wfs[0]+" > ul")
-print(1,lstcate)
-for temp in lstcate:
-    tempid=temp['id'][8:]
-    #print(tempid)
-    wfs.append(tempid)
-    result.append(tempid+":"+temp.find('a').text)
-    #print(temp['id'][8:])
-    #print(temp.find('a'))
-    # print(temp)
-wfs.pop(0)
 
-print(wfs)
+    ## Get all of <a>
+    LstDepth23 = cateLiArea.findAll('a')
+    for temp in LstDepth23:
+        result.append(str(temp['href'][len(removeStr)+1:]).strip() + ":" + str(temp.text).strip())
+        # todo : insert DB
+
+GetCategory('001')
+GetCategory('002')
 print(result)
