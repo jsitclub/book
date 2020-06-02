94f2ab43002521166a29f9b8748c4a7dde25c3cd
cat <aa@bb.cc>
2020-06-02 22:45:12 +0900

    get nvr_category

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+if tempcate==category:
+        #     result.append(tempcate+":"+temp.text)
+        #     return
+        # elif len(tempcate)>3:
+        #     GetCategory(tempcate)
+        # else:
+        
