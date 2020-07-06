7d3e22b3d7ce1b4a4645fd19ca8b7dcd0ae9881f
JS IT CLUB <jsitclub@gmail.com>
2020-07-06 03:23:45 +0000

    clear DB

--- MyBooks/Book/models.py
+++ MyBooks/Book/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Book(models.Model):
+    code=models.AutoField(primary_key=True,default='')
     title=models.CharField(max_length=150)
     author=models.CharField(max_length=50)
     translator=models.CharField(max_length=50,null=True,default='')
@@ -11,20 +12,17 @@ class Book(models.Model):
     publisher=models.CharField(max_length=50, null=True,default='')
     publishdate=models.DateField(null=True)
     created=models.DateTimeField(null=True)
-    
 
 class BookCover(models.Model):
-    bookcode=models.ForeignKey('Book', on_delete=models.CASCADE)
+    code=models.ForeignKey('Book', on_delete=models.CASCADE,default="")
     cover=models.ImageField(upload_to='book_covers/')
     created=models.DateTimeField(null=True)
-    
-    
+
 class Category(models.Model):
-    code=models.CharField(max_length=15,unique=True,null=False)
-    content=models.CharField(max_length=50)  
-
-
+    code=models.CharField(primary_key=True,default='',max_length=21)
+    content=models.CharField(max_length=50) 
+    
 class Book_Cate(models.Model):
-    bookcode=models.IntegerField(null=False)
-    catecode=models.CharField(max_length=15)
+    bookcode=models.ForeignKey('Book', on_delete=models.CASCADE,default="")
+    catecode=models.CharField(max_length=21)
     
